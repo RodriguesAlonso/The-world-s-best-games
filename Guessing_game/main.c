7e05ffee9c90da27c1d3da3d406efdd1af4a8440
Alonso <joaoalonso42@gmail.com>
2023-09-03 13:08:43 -0300

    add attempts

--- Guessing_game/main.c
+++ Guessing_game/main.c
@@ -9,7 +9,7 @@ void introduction()
     printf("\nThe objective of the game is to guess a specific number correctly.\n");
 }
 
-void checkGuess(int guess, int secretNumber)
+void checkGuess(int guess, int secretNumber, int attempts)
 {
     if (guess == secretNumber)
     {
@@ -23,23 +23,32 @@ void checkGuess(int guess, int secretNumber)
     {
         printf("\nYour guess is too high!\n");
     }
+    if (attempts == 1)
+    {
+        printf("\nYou ran out of attempts!\n");
+    }
+    
 }
 
 void playGuess()
 {
     int guess;
     int secretNumber = rand() % 100 + 1;
+    int attempts = 5;
     introduction();
 
     do
     {
+        printf("\nNumber of remaining attempts, attempts: %d\n", attempts);
         printf("\nTake a guess at a number: ");
         scanf("%d", &guess);
         printf("\nYour guess is %d\n", guess);
 
-        checkGuess(guess, secretNumber);
+        checkGuess(guess, secretNumber, attempts);
 
-    } while (guess != secretNumber);
+        attempts--;
+
+    } while (guess != secretNumber && attempts != 0);
 };
 
 int main()
